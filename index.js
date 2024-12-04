f00acbb0989efeecf43c3d622e86ee90eaacbcdc
innocent <innocent.wasia@studentmoringaschool.com>
2024-12-04 10:23:51 +0300

    destructuring-assignment

--- index.js
+++ index.js
@@ -25,24 +25,47 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
-
+// **Strings**
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+// 3. Little the chicken had to go back to the coop, so now we're left with three. 
+// Let's use color variables of blackAndWhite, black, and pink.
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
 
-// Arrays
-
+// **Arrays**
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+// 5. Some people have a really hard time picking out indigo, so let's leave that one out, 
+// using the first letter of each color as the variable names.
+const [r, o, y, g, b, , v] = colors;
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+const [, , , , , indg] = colors;
 
-// Objects
+// **Objects**
+// 7. Use destructuring to assign all variables using the keys as the variable names.
+const { muppetName, color, song, job, partner } = muppet;
 
-// 7. Use destructuring to assign all variables using the keys as the variable names
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner.
+const {
+  album: {
+    theMuppetMovie: { song2, song4 },
+  },
+  nestedJob,
+  nestedPartner,
+} = nestedMuppet;
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+// **Output to Verify**
+console.log(moo, neigh, baa, oink, cluck); // cow horse sheep pig chicken
+console.log(bessie, dolly, babe, little); // cow sheep pig chicken
+console.log(blackAndWhite, black, pink); // cow pig chicken
+console.log(red, orange, yellow, green, blue, indigo, violet); // red orange yellow green blue indigo violet
+console.log(r, o, y, g, b, v); // r o y g b v
+console.log(indg); // indigo
+console.log(muppetName, color, song, job, partner); // Miss Piggy pink Never Before, Never Again Cast member of The Muppet Show Kermit
+console.log(song2, song4, nestedJob, nestedPartner); // Moving Right Along I Hope That Something Better Comes Along Host of The Muppet Show Miss Piggy
